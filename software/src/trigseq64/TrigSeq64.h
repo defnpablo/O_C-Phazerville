960d9fa626fdaf36022324afab4f85e19e9aff67
Pablo <pablo@Pablos-MacBook-Pro.local>
2025-11-23 22:44:19 -0300

    WIP

--- software/src/trigseq64/TrigSeq64.h
+++ software/src/trigseq64/TrigSeq64.h
@@ -9,6 +9,26 @@ public:
   static constexpr std::size_t PAGE_LENGTH = 16;
   static constexpr std::size_t PAGE_COUNT  = 4;
 
+  TrigSeq64()
+  : steps_()
+  , page_cursor_(0)
+  , step_cursor_(0)
+  , playhead_cursor_(0)
+  , end_cursor_(15)
+  {}
+
+  TrigSeq64(const std::bitset<MAX_STEPS>& steps,
+            uint8_t page_cursor,
+            uint8_t step_cursor,
+            uint8_t playhead_cursor,
+            uint8_t end_cursor)
+  : steps_(steps)
+  , page_cursor_(page_cursor)
+  , step_cursor_(step_cursor)
+  , playhead_cursor_(playhead_cursor)
+  , end_cursor_(end_cursor)
+  {}
+
   const std::bitset<MAX_STEPS>& steps() const { return steps_; }
   uint8_t page_cursor()                 const { return page_cursor_; }
   uint8_t step_cursor()                 const { return step_cursor_; }
@@ -28,9 +48,9 @@ public:
   }
 
 private:
-  std::bitset<MAX_STEPS> steps_{};
-  uint8_t page_cursor_     = 0;
-  uint8_t step_cursor_     = 0;
-  uint8_t playhead_cursor_ = 0;
-  uint8_t end_cursor_      = 15;
+  std::bitset<MAX_STEPS> steps_;
+  uint8_t page_cursor_;
+  uint8_t step_cursor_;
+  uint8_t playhead_cursor_;
+  uint8_t end_cursor_;
 };
--- software/test/trigseq64_test.cpp
+++ software/test/trigseq64_test.cpp
@@ -1,54 +1,15 @@
-#include <cstdint>
-#include <bitset>
+#include "gtest/gtest.h"
+#include "trigseq64/TrigSeq64.h"
 
-class TrigSeq64 {
-public:
-  static constexpr std::size_t MAX_STEPS   = 64;
-  static constexpr std::size_t PAGE_LENGTH = 16;
-  static constexpr std::size_t PAGE_COUNT  = 4;
+TEST(TrigSeq64Test, InitialState_Defaults)
+{
+  TrigSeq64 t;
 
-  TrigSeq64()
-  : steps_()
-  , page_cursor_(0)
-  , step_cursor_(0)
-  , playhead_cursor_(0)
-  , end_cursor_(15)
-  {}
+  EXPECT_EQ(0,  t.page_cursor());
+  EXPECT_EQ(0,  t.step_cursor());
+  EXPECT_EQ(0,  t.playhead_cursor());
+  EXPECT_EQ(15, t.end_cursor());
 
-  TrigSeq64(const std::bitset<MAX_STEPS>& steps,
-            uint8_t page_cursor,
-            uint8_t step_cursor,
-            uint8_t playhead_cursor,
-            uint8_t end_cursor)
-  : steps_(steps)
-  , page_cursor_(page_cursor)
-  , step_cursor_(step_cursor)
-  , playhead_cursor_(playhead_cursor)
-  , end_cursor_(end_cursor)
-  {}
-
-  const std::bitset<MAX_STEPS>& steps() const { return steps_; }
-  uint8_t page_cursor()                 const { return page_cursor_; }
-  uint8_t step_cursor()                 const { return step_cursor_; }
-  uint8_t playhead_cursor()             const { return playhead_cursor_; }
-  uint8_t end_cursor()                  const { return end_cursor_; }
-
-  void reset() {
-    playhead_cursor_ = 0;
-  }
-
-  void advance_step() {
-    if (playhead_cursor_ >= end_cursor_) {
-      reset();
-    } else {
-      ++playhead_cursor_;
-    }
-  }
-
-private:
-  std::bitset<MAX_STEPS> steps_;
-  uint8_t page_cursor_;
-  uint8_t step_cursor_;
-  uint8_t playhead_cursor_;
-  uint8_t end_cursor_;
-};
+  EXPECT_EQ(64, t.steps().size());
+  EXPECT_TRUE(t.steps().none());
+}
