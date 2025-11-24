81baa7b572de2b3a64e75fe17fb619f55c05fd05
Pablo <pablo@Pablos-MacBook-Pro.local>
2025-11-24 13:27:25 -0300

    WIP

--- software/src/trigseq64/TrigSeq64.h
+++ software/src/trigseq64/TrigSeq64.h
@@ -75,6 +75,32 @@ public:
     end_cursor_ = page_cursor_ * PAGE_LENGTH + PAGE_LENGTH - 1;
   }
 
+  void advance_step_cursor() {
+    uint8_t page_start = page_cursor_ * PAGE_LENGTH;
+    uint8_t page_end = page_start + PAGE_LENGTH - 1;
+    
+    if (step_cursor_ >= page_end) {
+      step_cursor_ = page_start;
+    } else {
+      ++step_cursor_;
+    }
+  }
+
+  void retreat_step_cursor() {
+    uint8_t page_start = page_cursor_ * PAGE_LENGTH;
+    uint8_t page_end = page_start + PAGE_LENGTH - 1;
+    
+    if (step_cursor_ <= page_start) {
+      step_cursor_ = page_end;
+    } else {
+      --step_cursor_;
+    }
+  }
+
+  void toggle_step_cursor() {
+    steps_.flip(step_cursor_);
+  }
+
 private:
   std::bitset<MAX_STEPS> steps_;
   uint8_t page_cursor_;
--- software/test/trigseq64_test.cpp
+++ software/test/trigseq64_test.cpp
@@ -248,3 +248,139 @@ TEST(TrigSeq64Test, SetEndCursorToPageEnd_Page3_SetsTo63)
   t.set_end_cursor_to_page_end();
   EXPECT_EQ(63, t.end_cursor());
 }
+
+// Step cursor navigation tests
+TEST(TrigSeq64Test, AdvanceStepCursor_WithinPage_Increments)
+{
+  std::bitset<TrigSeq64::MAX_STEPS> steps;
+
+  TrigSeq64 t(steps,
+              0,
+              5,
+              63,
+              0);
+
+  t.advance_step_cursor();
+  EXPECT_EQ(6, t.step_cursor());
+}
+
+TEST(TrigSeq64Test, AdvanceStepCursor_AtPageEnd_WrapsToPageStart)
+{
+  std::bitset<TrigSeq64::MAX_STEPS> steps;
+
+  TrigSeq64 t(steps,
+              0,
+              15, // last step of page 0
+              63,
+              0);
+
+  t.advance_step_cursor();
+  EXPECT_EQ(0, t.step_cursor());
+}
+
+TEST(TrigSeq64Test, AdvanceStepCursor_Page2_AtPageEnd_WrapsToPageStart)
+{
+  std::bitset<TrigSeq64::MAX_STEPS> steps;
+
+  TrigSeq64 t(steps,
+              0,
+              47, // last step of page 2
+              63,
+              2);
+
+  t.advance_step_cursor();
+  EXPECT_EQ(32, t.step_cursor());
+}
+
+TEST(TrigSeq64Test, RetreatStepCursor_WithinPage_Decrements)
+{
+  std::bitset<TrigSeq64::MAX_STEPS> steps;
+
+  TrigSeq64 t(steps,
+              0,
+              10,
+              63,
+              0);
+
+  t.retreat_step_cursor();
+  EXPECT_EQ(9, t.step_cursor());
+}
+
+TEST(TrigSeq64Test, RetreatStepCursor_AtPageStart_WrapsToPageEnd)
+{
+  std::bitset<TrigSeq64::MAX_STEPS> steps;
+
+  TrigSeq64 t(steps,
+              0,
+              0, // first step of page 0
+              63,
+              0);
+
+  t.retreat_step_cursor();
+  EXPECT_EQ(15, t.step_cursor());
+}
+
+TEST(TrigSeq64Test, RetreatStepCursor_Page1_AtPageStart_WrapsToPageEnd)
+{
+  std::bitset<TrigSeq64::MAX_STEPS> steps;
+
+  TrigSeq64 t(steps,
+              0,
+              16, // first step of page 1
+              63,
+              1);
+
+  t.retreat_step_cursor();
+  EXPECT_EQ(31, t.step_cursor());
+}
+
+// Toggle step cursor tests
+TEST(TrigSeq64Test, ToggleStepCursor_FromFalse_BecomesTrue)
+{
+  std::bitset<TrigSeq64::MAX_STEPS> steps;
+
+  TrigSeq64 t(steps,
+              0,
+              5,
+              63,
+              0);
+
+  EXPECT_FALSE(t.steps()[5]);
+  t.toggle_step_cursor();
+  EXPECT_TRUE(t.steps()[5]);
+}
+
+TEST(TrigSeq64Test, ToggleStepCursor_FromTrue_BecomesFalse)
+{
+  std::bitset<TrigSeq64::MAX_STEPS> steps;
+  steps.set(10);
+
+  TrigSeq64 t(steps,
+              0,
+              10,
+              63,
+              0);
+
+  EXPECT_TRUE(t.steps()[10]);
+  t.toggle_step_cursor();
+  EXPECT_FALSE(t.steps()[10]);
+}
+
+TEST(TrigSeq64Test, ToggleStepCursor_MultipleTimes_Alternates)
+{
+  std::bitset<TrigSeq64::MAX_STEPS> steps;
+
+  TrigSeq64 t(steps,
+              0,
+              20,
+              63,
+              1);
+
+  EXPECT_FALSE(t.steps()[20]);
+  t.toggle_step_cursor();
+  EXPECT_TRUE(t.steps()[20]);
+  t.toggle_step_cursor();
+  EXPECT_FALSE(t.steps()[20]);
+  t.toggle_step_cursor();
+  EXPECT_TRUE(t.steps()[20]);
+}
