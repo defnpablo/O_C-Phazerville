d601833aa576ecc8f7df0c7245b78e63c6f2f314
Pablo <pablo@Pablos-MacBook-Pro.local>
2025-11-23 21:37:14 -0300

    WIP

--- software/src/trigseq64/TrigSeq64.h
+++ software/src/trigseq64/TrigSeq64.h
@@ -10,10 +10,10 @@ public:
   static constexpr std::size_t PAGE_COUNT  = 4;
 
   const std::bitset<MAX_STEPS>& steps() const { return steps_; }
-  uint8_t page_cursor() const { return page_cursor_; }
-  uint8_t step_cursor() const { return step_cursor_; }
-  uint8_t playhead_cursor() const { return playhead_cursor_; }
-  uint8_t end_cursor() const { return end_cursor_; }
+  uint8_t page_cursor()                 const { return page_cursor_; }
+  uint8_t step_cursor()                 const { return step_cursor_; }
+  uint8_t playhead_cursor()             const { return playhead_cursor_; }
+  uint8_t end_cursor()                  const { return end_cursor_; }
 
 private:
   std::bitset<MAX_STEPS> steps_{};
